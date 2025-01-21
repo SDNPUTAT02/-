23572738a81dd65c52a34bb6449adafbda64ee40
SDNPUTAT02 <a4diiikkaaa@gmail.com>
2025-01-21 12:22:51 +0700

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -7,7 +7,7 @@ function updateDateTime() {
     const dateTimeElement = document.getElementById('date-time');
     const now = new Date();
 
-    const days = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+    
     const months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
 
     const dayOfWeek = days[now.getDay()];
